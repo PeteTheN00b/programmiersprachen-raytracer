c2f9ab5fed264dbf2f3f5bbdf084d745624bd240
PeteTheN00b <ropeter63@googlemail.com>
2020-07-19 00:30:20 +0200

    Added area and volume overload for sphere

---
+++ framework/sphere.cpp
@@ -0,0 +1,19 @@
+#include "sphere.hpp"
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
+Sphere::Sphere(glm::vec3 centre, float radius) :
+	centre_{centre},
+	radius_{radius} {}
+
+float Sphere::area() const
+{
+	return M_PI * pow(radius_, 2);
+}
+
+float Sphere::volume() const
+{
+	return 4.f / 3.f * M_PI * pow(radius_, 3);
+}
---
+++ framework/sphere.hpp
@@ -0,0 +1,21 @@
+#ifndef SPHERE_HPP
+#define SPHERE_HPP
+
+#include <glm/vec3.hpp>
+
+#include "shape.hpp"
+
+class Sphere : public Shape
+{
+public:
+	Sphere(glm::vec3 centre, float radius);
+
+	float area() const override;
+	float volume() const override;
+
+protected:
+	glm::vec3 centre_;
+	float radius_;
+};
+
+#endif
