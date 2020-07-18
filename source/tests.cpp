cad3b35a91160cd36db6920c9e955d9581dfda73
PeteTheN00b <ropeter63@googlemail.com>
2020-07-19 00:29:59 +0200

    Added shape, sphere and box to CMake for tests

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,17 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#define _USE_MATH_DEFINES //for pi
+#include <math.h>
+
+#include "sphere.hpp"
+#include "box.hpp"
+
+TEST_CASE("test")
+{
+    Sphere s{ {0, 0, 0}, 5 }; //sphere with centre at origin and radius of 5
+    REQUIRE(s.area() == Approx(25.f * M_PI));
+    REQUIRE(s.volume() == Approx(125.f * 4.f / 3.f * M_PI));
+}
 
 int main(int argc, char *argv[])
 {
