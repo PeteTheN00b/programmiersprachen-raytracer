12cc09b5e52c73309e181bdc93f919d4ec9bb2c0
PeteTheN00b <ropeter63@googlemail.com>
2020-07-19 19:27:54 +0200

    Sphere / ray intersections tested

--- source/tests.cpp
+++ source/tests.cpp
@@ -6,6 +6,91 @@
 #include "sphere.hpp"
 #include "box.hpp"
 
+# include <glm/glm.hpp>
+# include <glm/gtx/intersect.hpp>
+TEST_CASE("intersect_ray_sphere", "[intersect]")
+{
+    // Ray
+    glm::vec3 ray_origin{ 0.0f, 0.0f, 0.0f };
+    // ray direction has to be normalized !
+    // you can use :
+    // v = glm :: normalize ( some_vector )
+    glm::vec3 ray_direction{ 0.0f, 0.0f, 1.0f };
+    // Sphere
+    glm::vec3 sphere_center{ 0.0f ,0.0f, 5.0f };
+    float sphere_radius{ 1.0f };
+    float distance = 0.0f;
+    auto result = glm::intersectRaySphere(
+        ray_origin, ray_direction,
+        sphere_center,
+        sphere_radius * sphere_radius, // squared radius !!!
+        distance);
+    REQUIRE(distance == Approx(4.0f));
+}
+
+TEST_CASE("intersect function")
+{
+    SECTION("Ray moves in the x direction, intersects sphere at the centre")
+    {
+        Ray r{ {0, 0, 0}, {1, 0, 0} };
+        Sphere s{ "TestSphere", {1, 0, 0}, {50, 0, 0}, 1.f };
+
+        HitPoint h = s.intersect(r);
+
+        REQUIRE(h.intersect);
+        REQUIRE(Approx(49.f) == h.dist);
+        REQUIRE("TestSphere" == h.objName);
+        REQUIRE(Approx(1.f) == h.objColor.r);
+        REQUIRE((Approx(49.f) == h.intersectPoint.x && Approx(0.f) == h.intersectPoint.y && Approx(0.f) == h.intersectPoint.z));
+        REQUIRE(h.rayDirection == r.direction);
+    }
+
+    SECTION("Ray moves away from sphere, no intersection occurs")
+    {
+        Ray r{ {0, 0, 0}, {-1, 0, 0} };
+        Sphere s{ "TestSphere", {1, 0, 0}, {50, 0, 0}, 1.f };
+
+        HitPoint h = s.intersect(r);
+
+        REQUIRE(!h.intersect);
+    }
+
+    SECTION("Ray intersects at tangent of sphere")
+    {
+        Ray r{ {0, 1, 0}, {1, 0, 0} };
+        Sphere s{ "TestSphere", {1, 0, 0}, {50, 0, 0}, 1.f };
+
+        HitPoint h = s.intersect(r);
+
+        REQUIRE(h.intersect);
+        REQUIRE(Approx(50.f) == h.dist);
+        REQUIRE((Approx(50.f) == h.intersectPoint.x && Approx(1.f) == h.intersectPoint.y && Approx(0.f) == h.intersectPoint.z));
+    }
+
+    SECTION("Ray starts inside of sphere, appears to intersect at the circle's surface")
+        //testing confirms peculiar interaction, an intersection is only detected on the outside of the circle, not on the inside
+    {
+
+        Ray r{ {0, 0, 0}, {1, 0, 0} };
+        Sphere s{ "TestSphere", {1, 0, 0}, {0, 0, 0}, 5.f };
+
+        HitPoint h = s.intersect(r);
+
+        REQUIRE(h.intersect);
+        REQUIRE(Approx(5.f) == h.dist);
+    }
+
+    SECTION("Ray intersects with diagonal movement")
+    {
+        Ray r{ {0, 0, 0}, {1, 2, 3} };
+        Sphere s{ "TestSphere", {1, 0, 0}, {3, 6, 9}, 2.0f };
+
+        HitPoint h = s.intersect(r);
+
+        REQUIRE(h.intersect);
+    }
+}
+
 TEST_CASE("Sphere area and volume functions")
 {
     Sphere s{ "MySphere", {1, 0, 0}, {0, 0, 0}, 5 }; //sphere with centre at origin and radius of 5
@@ -26,5 +111,5 @@ TEST_CASE("Box area and volume functions")
 
 int main(int argc, char *argv[])
 {
-  return Catch::Session().run(argc, argv);
+    return Catch::Session().run(argc, argv);
 }
