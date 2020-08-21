ed7e2e2d28e240bc4c6623afe134abd737c6aa05
PeteTheN00b <ropeter63@googlemail.com>
2020-08-22 00:44:45 +0200

    Preparations for camera options

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -19,6 +19,19 @@
 #include "material.hpp"
 #include "world.hpp"
 
+Color operator*(Color const& color, float f) //the color struct that was given didn't have an overload for multiplication, so I made one here
+{
+    return { color.r * f, color.g * f, color.b * f };
+}
+
+Color operator*=(Color& color, float f)
+{
+    color = color * f;
+    return color;
+}
+
+//specular is being initialized with centre point values? specularExp is bugged?
+
 //now single threaded again
 int main(int argc, char* argv[])
 {
@@ -31,22 +44,21 @@ int main(int argc, char* argv[])
   renderer.render();
 
   Color const bgColor{0.1f, 0.4f, 0.2f}; //background color
-  glm::vec3 const observerLoc{ 0.f, 0.f, 0.f };
 
 
   World world{};
-  /*world.createMaterial("Red", { 0.7f, 0.f, 0.2f }, 0.3f, 0.8f, 0.2f);
-  world.createMaterial("Purple", { 0.7f, 0.1f, 0.9f }, 0.3f, 0.3f, 0.9f);
-  world.createMaterial("Table", { 0.7f, 0.6f, 0.f }, 0.3f, 0.7f, 1.f);
-  world.createMaterial("Cyan", { 0.f, 0.9f, 0.8f }, 0.3f, 1.f, 0.5f);
+  /*world.createMaterial("Red", { 0.7f, 0.f, 0.2f }, { 0.7f, 0.f, 0.2f }, { 0.7f, 0.f, 0.2f }, 50);
+  world.createMaterial("Purple", { 0.7f, 0.1f, 0.9f }, { 0.7f, 0.1f, 0.9f }, { 0.7f, 0.1f, 0.9f }, 50);
+  world.createMaterial("Table", { 0.7f, 0.6f, 0.f }, { 0.7f, 0.6f, 0.f }, { 0.7f, 0.6f, 0.f }, 50);
+  world.createMaterial("Cyan", { 0.f, 0.9f, 0.8f }, { 0.f, 0.9f, 0.8f }, { 0.f, 0.9f, 0.8f }, 50);
 
   world.createSphere("Red", { 0.f, 0.f, -150.f }, 50.f);
   world.createSphere("Purple", { -20.f, -70.f, -150.f }, 80.f);
 
   world.createBox("Table", { 0.f, -140.f, -300.f }, 500.f, 20.f, 500.f);
-  world.createBox("Cyan", { 150.f, -120.f, -100.f }, 100.f, 40.f, 60.f);*/
+  world.createBox("Cyan", { 150.f, -120.f, -100.f }, 100.f, 40.f, 60.f);
 
-  /*world.createLight({ 3000.f, 500.f, -150.f }, 1.f);
+  world.createLight({ 3000.f, 500.f, -150.f }, 1.f);
   world.createLight({ -3000.f, 500.f, -150.f }, 1.f);*/
 
   std::ifstream freader;
@@ -68,11 +80,17 @@ int main(int argc, char* argv[])
           if ("material" == identifier)
           {
               std::string matName;
-              float colorR, colorG, colorB, ambient, diffusive, specular;
+              Color ambient, diffusive, specular;
+              int specularExp;
 
-              in_sstream >> matName >> colorR >> colorG >> colorB >> ambient >> diffusive >> specular;
+              in_sstream >> 
+              matName >> 
+              ambient.r >> ambient.g >> ambient.b >> 
+              diffusive.r >> diffusive.g >> diffusive.b >> 
+              specular.r >> specular.g >> specular.b >> 
+              specularExp;
 
-              world.createMaterial(matName, { colorR, colorG, colorB }, ambient, diffusive, specular);
+              world.createMaterial(matName, ambient, diffusive, specular, specularExp);
           }
           else if("shape" == identifier)
           {
@@ -121,72 +139,77 @@ int main(int argc, char* argv[])
   {
       for (unsigned int j = 0; j < image_height; j++)
       {
-          HitPoint closestHit;
-
-          for (std::shared_ptr<Shape> s : world.getShapes())
-          {
-              //use field of view angle from filereading to determine min / max ratio between (x and z) / (y and z) coordinates (45 degrees is a 1 : 1 ratio)
-              float const maxAngle = 80.f;
-              glm::vec3 direction{
-                  ((float)i / (float)image_width - 0.5f) * 2 * (float)tan(maxAngle / 180.f * M_PI),
-                  /*(float)j - (float)image_height / 2.f*/
-                  ((float)j / (float)image_width - 0.5f) * 2 * (float)tan(maxAngle / 180.f * M_PI),
-                  -1.f };
-              direction = glm::normalize(direction);
-
-              HitPoint h = s.get()->intersect(Ray{ observerLoc,  direction});
-
-              if (h.intersect && (h.dist < closestHit.dist || !closestHit.intersect))
-              {
-                  closestHit = h;
-              }
-          }
-
           //by default our pixel has the background color
           Color pColor = bgColor;
-          
-          //if an object was intersected we use that instead
-          if (closestHit.intersect)
+
+          float const fovX = 80.f;
+          float const fovY = 80.f;
+          float x = ((float)i / (float)image_width - 0.5f) * 2 * (float)tan(fovX / 180.f * M_PI);
+          float y = ((float)j / (float)image_width - 0.5f) * 2 * (float)tan(fovY / 180.f * M_PI);
+
+          glm::vec3 const dir{ 0.f, 0.f, -1.f }; //-z direction (camera forward)
+          glm::vec3 const up{ 0.f, 1.f, 0.f }; //y direction
+          glm::vec3 const right = glm::cross(dir, up); //x direction
+
+          glm::vec3 direction = x * right + y * up + dir;
+
+          glm::vec3 const origin{ 0.f, 0.f, 0.f };
+
+          //convert below into recursive function
           {
-              pColor = closestHit.objMat.color;
+              HitPoint closestHit;
 
-              float intensity = closestHit.objMat.ambient * ambientIntensity;
-
-              for (std::shared_ptr<PointLight> l : world.getLights())
+              for (std::shared_ptr<Shape> s : world.getShapes())
               {
-                  int notObstructed = 1; //assume that our light isn't obstructed unless proven otherwise
-
-                  glm::vec3 direction{ {l.get()->origin - closestHit.intersectPoint} };
                   direction = glm::normalize(direction);
 
-                  for (std::shared_ptr<Shape> s : world.getShapes()) //check whether or not our ray is obstructed by the light source
+                  HitPoint h = s.get()->intersect(Ray{ origin,  direction }); //CAUSES CRASHES, unrelated to filereading
+
+                  if (h.intersect && (h.dist < closestHit.dist || !closestHit.intersect))
                   {
-                      if (s.get()->intersect(Ray{ closestHit.intersectPoint + closestHit.objNormal, direction }).intersect)
-                          //adding normal to make sure it doesn't re-intersect with the intersection point
-                      {
-                          notObstructed = 0;
-                          break;
-                      }
+                      closestHit = h;
                   }
-
-                  float dotProduct1 = abs(glm::dot<float>(closestHit.objNormal, direction));
-
-                  glm::vec3 reflectedLightDirection = closestHit.intersectPoint + closestHit.objNormal - l.get()->origin;
-                  reflectedLightDirection *= 2;
-                  reflectedLightDirection -= closestHit.intersectPoint;
-                  reflectedLightDirection = glm::normalize(reflectedLightDirection);
-
-                  glm::vec3 reverseOrigin = observerLoc - closestHit.intersectPoint;
-                  reverseOrigin = glm::normalize(reverseOrigin);
-                  float dotProduct2 = pow(abs(glm::dot<float>(reflectedLightDirection, reverseOrigin)), 5);
-                  intensity += l.get()->intensity * (closestHit.objMat.diffusive * (float)notObstructed * dotProduct1 + closestHit.objMat.specular * dotProduct2);
               }
 
-              intensity /= (intensity + 1);
+              //if an object was intersected we use that instead
+              if (closestHit.intersect)
+              {
+                  pColor = closestHit.objMat.ambient * ambientIntensity;
 
-              pColor.r *= intensity;
-              pColor.g *= intensity;
-              pColor.b *= intensity;
+                  for (std::shared_ptr<PointLight> l : world.getLights())
+                  {
+                      int notObstructed = 1; //assume that our light isn't obstructed unless proven otherwise
+
+                      glm::vec3 direction{ {l.get()->origin - closestHit.intersectPoint} };
+                      direction = glm::normalize(direction);
+
+                      for (std::shared_ptr<Shape> s : world.getShapes()) //check whether or not our ray is obstructed by the light source
+                      {
+                          if (s.get()->intersect(Ray{ closestHit.intersectPoint + closestHit.objNormal, direction }).intersect)
+                              //adding normal to make sure it doesn't re-intersect with the intersection point
+                          {
+                              notObstructed = 0;
+                              break;
+                          }
+                      }
+
+                      float dotProduct1 = abs(glm::dot<float>(closestHit.objNormal, direction));
+
+                      glm::vec3 reflectedLightDirection = closestHit.intersectPoint + closestHit.objNormal - l.get()->origin;
+                      reflectedLightDirection *= 2;
+                      reflectedLightDirection -= closestHit.intersectPoint;
+                      reflectedLightDirection = glm::normalize(reflectedLightDirection);
+
+                      glm::vec3 reverseOrigin = origin - closestHit.intersectPoint;
+                      reverseOrigin = glm::normalize(reverseOrigin);
+                      float dotProduct2 = pow(abs(glm::dot<float>(reflectedLightDirection, reverseOrigin)), 5);
+                      pColor += (closestHit.objMat.diffusive * (float)notObstructed * dotProduct1 + closestHit.objMat.specular * pow(dotProduct2, closestHit.objMat.specularExp)) * l.get()->intensity;
+                  }
+
+                  pColor.r /= (pColor.r + 1.f);
+                  pColor.g /= (pColor.g + 1.f);
+                  pColor.b /= (pColor.b + 1.f);
+              }
           }
 
           Pixel p{ i, j, pColor };
