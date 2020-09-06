9e7da2c3be0b5b26734a47a48cabaf069733b5fa
PeteTheN00b <ropeter63@googlemail.com>
2020-09-06 18:12:17 +0200

    Cleaned up comments and fixed an issue where specular lighting was being raised to the specular exponent twice

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -60,7 +60,7 @@ void makeAnimationSdf(int iteration, int maxIteration)
     ofs << "define shape sphere testSphere 0 -70 -150 80 purple" << std::endl << std::endl
         << "define shape box table -500 -160 -3300 500 -140 2700 table" << std::endl
         << "define shape box testBox -50 -140 -40 -350 -50 180 blue" << std::endl << std::endl
-        << "define shape composite testComposite testSphere testBox" << std::endl << std::endl
+        << "define shape composite testComposite testSphere testBox table" << std::endl << std::endl
         << "define light -3000 1000 -1500 1.0 ";
 
     if (iteration < 90) ofs << "1.0 1.0";
@@ -77,7 +77,7 @@ void makeAnimationSdf(int iteration, int maxIteration)
 
 
     ofs << "define camera cam1 60 ";
-    if (iteration >= 70) ofs << "500 500 0 -7 -1 0 1 7 0";
+    if (iteration >= 70) ofs << "500 500 -500 -7 -1 0 1 7 0";
     else if (iteration < 40) ofs << "0 200 500 0 -1 -7 0 7 1";
     else
     {
@@ -102,15 +102,16 @@ void makeAnimationSdf(int iteration, int maxIteration)
         sphereScale << std::fixed << std::setprecision(2) << 1.f + (float)(iteration) / 19.f; //from 1 to 2
         ofs << sphereScale.str() + " " + sphereScale.str() + " " + sphereScale.str();
     }
+    ofs << std::endl;
 
-    if (iteration >= 90) ofs << "transform testComposite rotate -90 0 0 1";
+    if (iteration >= 90) ofs << "transform testComposite translate 0 300 0";
     else if (iteration >= 70)
     {
         float const completion = (float)(iteration - 70.f) / 19.f;
 
         std::stringstream compositeRotation;
-        compositeRotation << std::fixed << std::setprecision(2) << completion * -90.f << " 0 0 1";
-        ofs << "transform testComposite rotate " << compositeRotation.str();
+        compositeRotation << std::fixed << std::setprecision(2) << completion * 300.f;
+        ofs << "transform testComposite translate 0 " << compositeRotation.str() << " 0";
     }
 }
 
@@ -292,97 +293,110 @@ void raytrace(int iteration, int maxIteration)
     ofs << "P6" << std::endl << image_width << ' ' << image_height << std::endl << "255" << std::endl;
 
     //This is how we write to the renderer
-    for (int j = image_height - 1; j >= 0; --j) //swapped these around for file reading, that's why i and j are swapped (j has to count backwards for ppm writing)
-        //j is an integer and not an unsigned, because when unsigned this loop cannot reach negatives and will therefore infinitely repeat unless an unintuitive condition is used
+    for (int i = image_height - 1; i >= 0; --i)
+        //i is an integer and not an unsigned, because when unsigned this loop cannot reach negatives and will therefore infinitely repeat unless an unintuitive condition is used
     {
-        for (unsigned int i = 0; i < image_width; ++i)
+        for (unsigned int j = 0; j < image_width; ++j)
         {
             //by default our pixel has the background color
             Color pColor = { 0.f, 0.f, 0.f };
 
-            float x = ((float)i / (float)image_width - 0.5f) * 2 * (float)tan(fovX / 180.f * M_PI);
-            float y = ((float)j / (float)image_height - 0.5f) * 2 * (float)tan(fovY / 180.f * M_PI);
+            float y = ((float)i / (float)image_height - 0.5f) * 2 * (float)tan(fovY / 180.f * M_PI); //how far along the y-axis (up) should we move for every unit along the z-axis (direction)
+            float x = ((float)j / (float)image_width - 0.5f) * 2 * (float)tan(fovX / 180.f * M_PI); //how far along the x-axis (right) should we move for every unit along the z-axis
 
-            glm::vec3 const right = glm::cross(dir, up); //x direction
+            glm::vec3 const right = glm::cross(dir, up); //calculate the x direction by finding the axis perpendicular to both our y and z axis
 
-            glm::vec3 direction = x * right + y * up + dir;
-            direction = glm::normalize(direction);
+            glm::vec3 direction = x * right + y * up + dir; //calculate the direction of the ray we will trace by adding a movement along the z-axis to our pixel based movement along the x and y axis
+            direction = glm::normalize(direction); //normalize direction, our calculations using directions and normals will always assume they have been normalized beforehand
 
             glm::vec3 origin = observerLoc;
 
             int reflectionCount = 0;
-            int maxReflectionCount = 10;
+            int maxReflectionCount = 10; //limit our number of reflections in the case of an error or an infinite loop caused by a camera within a cage of reflective surfaces
             int refractionCount = 0;
 
             //lambda for lighting using reflections (had to use std::function because you can't initialize a lambda recursively)
+                //we use a lambda because reflection and refraction will require recursive lighting calculations
             std::function<void(float, float)> lighting = [&](float reflectivity, float refractivity) //reflectivity of previous material, 1 for first material
             {
-                HitPoint closestHit;
+                HitPoint closestHit; //first we must find the closest hit traced by our ray
 
+                //to do so we will check whether it intersects with any shape, and if it does compare the distance to see if this object hit is closer than a previously determined one
                 for (std::shared_ptr<Shape> s : world.getShapes())
                 {
                     Ray r{ origin, direction };
 
-                    HitPoint h = s.get()->intersect(r);
+                    HitPoint h = s.get()->intersect(r); //transformations are handled within the intersect function
 
-                    if (h.intersect && (h.dist < closestHit.dist || !closestHit.intersect))
+                    if (h.intersect && (h.dist < closestHit.dist || !closestHit.intersect)) //check if distance is shorter than previous hit or if there was a previous hit
                     {
                         closestHit = h;
                     }
                 }
 
-                //if an object was intersected we use that instead
+                //if an intersection occurred we can use calculations to predict the color formed by the lighting at that point
                 if (closestHit.intersect)
                 {
-                    reflectivity *= (1.f - closestHit.objMat.reflectivity); //reduce own material intensity in exchange for adding reflected objects intensity
+                    //reflective and refractive materials will show fewer of their own properties to show the properties of the reflected / refracted objects they display
+                    reflectivity *= (1.f - closestHit.objMat.reflectivity);
                     refractivity *= (1.f - closestHit.objMat.refractivity);
 
-                    if (0 == reflectionCount && 0 == refractionCount) pColor = { 0.f, 0.f, 0.f };
+                    //add our object's ambient color which should always be present to our material
                     pColor += closestHit.objMat.ambient * ambientIntensity *
                         reflectivity * refractivity;
 
+                    //determine our remaining shading based on the individual lights
                     for (std::shared_ptr<PointLight> l : world.getLights())
                     {
-                        int notObstructed = 1; //assume that our light isn't obstructed unless proven otherwise
+                        bool notObstructed = true; //assume that our lighting isn't obstructed unless proven otherwise (if it obstructed we should notice a shadow)
 
+                        //we have to find the direction from the point our ray hit, to the light source (and once again normalize it)
                         glm::vec3 direction{ {l.get()->origin - closestHit.intersectPoint} };
                         direction = glm::normalize(direction);
 
-                        for (std::shared_ptr<Shape> s : world.getShapes()) //check whether or not our ray is obstructed by the light source
+                        //we use a similar method to before to check if any object in the scene is obstructing the light source from this point
+                        for (std::shared_ptr<Shape> s : world.getShapes())
                         {
+                            //we add the normal to our intersection to avoid cases where our hit would cause the object to block its own lighting
                             Ray r{ closestHit.intersectPoint + closestHit.objNormal, direction };
 
+                            //in this case we don't need to save any information about the intersection, we simply need to know if an intersection occurred
                             if (s.get()->intersect(r).intersect)
-                                //adding normal to make sure it doesn't re-intersect with the intersection point
                             {
-                                notObstructed = 0;
+                                //if it did our light source is obstructed, and thus diffusive and specular lighting will be ineffective
+                                notObstructed = false;
                                 break;
                             }
                         }
 
-                        float dotProduct1 = glm::dot<float>(closestHit.objNormal, direction);
-                        if (dotProduct1 < 0.f) dotProduct1 = 0.f;
+                        if (notObstructed) //if our lighting isn't blocked, perform diffusive and specular calculations
+                        {
+                            //find the angle between our normal, and the direction from the object to the light source (this is the dot product we will use for our diffusive lighting)
+                            float dotProduct1 = glm::dot<float>(closestHit.objNormal, direction);
+                            if (dotProduct1 < 0.f) dotProduct1 = 0.f; //an angle below 0 would produce negative lighting, we don't want that
 
+                            //we want to find the direction light is reflected in, by rotating it 180 degrees around the normal and inverting it (necessary for specular lighting)
+                            glm::vec3 lightDirection = closestHit.intersectPoint + closestHit.objNormal - l.get()->origin;
 
-                        glm::vec3 lightDirection = closestHit.intersectPoint + closestHit.objNormal - l.get()->origin;
+                            glm::vec4 lightDir4{ lightDirection.x, lightDirection.y, lightDirection.z, 1.f };
+                            glm::vec3 rotAxis = closestHit.objNormal;
+                            glm::mat4 rotMat = glm::rotate((float)M_PI, rotAxis); //rotate 180 degrees around the normal
+                            glm::vec4 reflectedLightDir4 = rotMat * -lightDir4;
 
-                        glm::vec4 lightDir4{ lightDirection.x, lightDirection.y, lightDirection.z, 1.f };
-                        glm::vec3 rotAxis = closestHit.objNormal;
-                        glm::mat4 rotMat = glm::rotate((float)M_PI, rotAxis); //rotate 180 degrees around the normal
-                        glm::vec4 reflectedLightDir4 = rotMat * -lightDir4;
+                            glm::vec3 reflectedLightDirection{ reflectedLightDir4 };
+                            reflectedLightDirection = glm::normalize(reflectedLightDirection);
 
-                        glm::vec3 reflectedLightDirection{ reflectedLightDir4 };
-                        reflectedLightDirection = glm::normalize(reflectedLightDirection);
+                            //we also want to find the simple direction from the object to our observer, to use in combination with our reflected light direction to produce specular lighting
+                            glm::vec3 reverseOrigin = origin - closestHit.intersectPoint;
+                            reverseOrigin = glm::normalize(reverseOrigin);
 
-                        glm::vec3 reverseOrigin = origin - closestHit.intersectPoint;
-                        reverseOrigin = glm::normalize(reverseOrigin);
+                            //our dot product for specular lighting must be raised to the power of the specular exponent to reduce the area it is applied to
+                            float dotProduct2 = pow(abs(glm::dot<float>(reflectedLightDirection, reverseOrigin)), closestHit.objMat.specularExp);
 
-                        float dotProduct2 = pow(abs(glm::dot<float>(reflectedLightDirection, reverseOrigin)), closestHit.objMat.specularExp);
-                        if (dotProduct2 < 0.f) dotProduct2 = 0.f;
-
-                        pColor += (closestHit.objMat.diffusive * (float)notObstructed * dotProduct1 + closestHit.objMat.specular * pow(dotProduct2, closestHit.objMat.specularExp))
-                            * l.get()->color * l.get()->intensity
-                            * reflectivity * refractivity;
+                            pColor += (closestHit.objMat.diffusive * dotProduct1 + closestHit.objMat.specular * dotProduct2)
+                                * l.get()->color * l.get()->intensity
+                                * reflectivity * refractivity;
+                        }
                     }
 
 
@@ -471,35 +485,35 @@ void raytrace(int iteration, int maxIteration)
             pColor.g /= (pColor.g + 1.f);
             pColor.b /= (pColor.b + 1.f);
 
-            Pixel p{ i, (unsigned)j, pColor };
+            Pixel p{ j, (unsigned)i, pColor };
             renderer.write(p);
 
             ofs << (char)((int)(pColor.r * 255) % 256) << (char)((int)(pColor.g * 255) % 256) << (char)((int)(pColor.b * 255) % 256);
             //floats range between 0 and 1, we want to change that to between 0 and 255, (needs to use char so every character represents one value between 0 and 255)
         }
 
-        std::cout << ((float)(image_height - j - 1) / (float)(image_height * (maxIteration + 1))
+        std::cout << ((float)(image_height - i - 1) / (float)(image_height * (maxIteration + 1))
             + (float)iteration / (float)(maxIteration + 1)) * 100.f << "% completed" << std::endl;
     }
 
-    //Window window{ {image_width, image_height} };
+    Window window{ {image_width, image_height} };
 
     ofs.close();
 
-    /*while (!window.should_close()) {
+    while (!window.should_close()) {
         if (window.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             window.close();
         }
         window.show(renderer.color_buffer());
-    }*/
+    }
 }
 
 
 //now single threaded again
 int main(int argc, char* argv[])
 {
-    int const frames = 90;
-    for (int i = 70; i < frames; ++i)
+    int const frames = 36;
+    for (int i = 35; i < frames; ++i)
     {
         makeAnimationSdf(i, frames - 1);
         raytrace(i, frames - 1);
